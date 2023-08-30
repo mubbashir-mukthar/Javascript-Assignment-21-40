8bd62cc619ee561a7291eea93fa76a9beede8381
mubbashir-mukthar <mubbashirmukthar@gmail.com>
2023-08-30 06:05:28 -0700

    working on the assignment

---
+++ app.js
@@ -0,0 +1,79 @@
+var userinput='Mubbashir'
+var alllower=userinput.toLowerCase()
+console.log(alllower)
+//question2
+var x='MUBBASHIR'
+x= x.toLowerCase()
+// console.log(x)
+//question3
+var y='mubbashir'
+y= y.toUpperCase()
+// console.log(y)
+//question4
+var c='adil'
+var c2=c.toLowerCase()
+// console.log(c2)
+//question5
+var d='hasseb'
+alert(d.toUpperCase())
+//question6
+var cityName="kaRachi"
+var m = cityName.slice(0,1).toUpperCase() + cityName.slice(1).toLowerCase()
+// console.log(m)
+
+//chap22-25
+var sameWords="captain"
+var f=sameWords.slice(1,3)
+// console.log(f)
+
+
+var numOfChracs = "abid"
+var numOfChracsLen = numOfChracs.length
+console.log(numOfChracsLen)
+
+//ques3
+var animal="elephant"
+var seg= animal.slice(2,6)
+// console.log(seg)
+
+//ques4
+var numOfChracs2="kaleem"
+var numOfChracsLen2=numOfChracsLen2
+//console.log(numOfChracs2)
+
+//ques5
+var str1 ="muzzamil"
+var strLen1 = str1.length
+var finalVar =str1.slice (0,1) + str1.slice(5,8)
+console.log(finalVar)
+
+//ques6
+
+var text = "To be or not to be.";
+var indx = text.indexOf("be");
+//console.log(indx)
+//ques7
+ var text = "To be or not to be.";
+var indx = text.lastIndexOf("be");
+//console.log(indx)
+
+//ques8
+var str2="go"
+var str3=str2.indexOf('o')
+//console.log(str3)
+
+//ques9
+
+var indexNum ="slaeem"
+if (indexNum.indexOf("ae") === 2) true 
+//console.log(indexNum)
+//ques10
+var num="a,b,c,d,e"
+var num1=num.charAt(2)
+console.log(num1)
+
+
+
+ 
+   
+
